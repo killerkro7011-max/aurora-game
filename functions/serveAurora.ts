ce8fe082c2a85381089a0b60654bfac54a13e436
Marion Harris <killerkro7011@gmail.com>
2026-05-12 21:17:17 +0000

    Auto-commit agent changes

---
+++ functions/serveAurora.ts
@@ -0,0 +1,4 @@
+// This file is auto-generated - see serveAurora.ts in root
+export default async function handler(req: Request): Promise<Response> {
+  return new Response("Loading...", { headers: { 'Content-Type': 'text/html' } });
+}
